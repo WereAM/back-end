5307692dbeeb15fec75c37d88037ede93bd38b3f
WereAM <michelle.ambunya@strathmore.edu>
2024-04-16 22:23:45 +0300

    feat: DELETE
    delete a cat's name

--- controllers/controller/cat.controller.js
+++ controllers/controller/cat.controller.js
@@ -17,13 +17,13 @@ exports.update = (req, res) => {
     res.send("Cat updated: " + name); //send a response to the client with the message "Cat updated"
 };
 
-// exports.delete = (req, res) => {
-//     const { name } = req.params; //extract the name parameter from the URL
-//     const index = cat.indexOf(name); //find the index of the cat with the name
-//     if (index !== -1) { //if the cat is found
-//         cat.splice(index, 1); //remove the cat from the array
-//         res.send("Cat deleted: " + name); //send a response to the client with the message "Cat deleted"
-//     } else {
-//         res.send("Cat not found: " + name); //send a response to the client with the message "Cat not found"
-//     }
-// };
+exports.delete = (req, res) => {
+    const { name } = req.params; //extract the name parameter from the URL
+    const index = cat.indexOf(name); //find the index of the cat with the name
+    if (index !== -1) { //if the cat is found
+        cat.splice(index, 1); //remove the cat from the array
+        res.send("Cat deleted: " + name); //send a response to the client with the message "Cat deleted"
+    } else {
+        res.send("Cat not found: " + name); //send a response to the client with the message "Cat not found"
+    }
+};
--- routes/cat.routes.js
+++ routes/cat.routes.js
@@ -5,6 +5,6 @@ const catController = require('../controllers/controller/cat.controller');
 router.post('/add/:name', catController.create); //add a cat to the array
 router.get('/', catController.read); //send cat names to the client
 router.put('/update/:name', catController.update); //update a cat
-// router.delete('/delete/:name', catController.delete); //delete a cat
+router.delete('/delete/:name', catController.delete); //delete a cat
 
 module.exports = router;
